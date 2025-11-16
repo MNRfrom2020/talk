48a9b3add7f410a7fe707bcbc9495a3c6a01f9e8
MNR from 2020 <contact.mnrfrom2020@gmail.com>
2025-11-16 09:35:41 +0000

    লাস্ট যে সেকশন থাকে, এইটা মোবাইল ভার্সনে নিচের দিকে হারিয়ে যায়। এখানে এত

--- src/components/podcasts/PodcastLibrary.tsx
+++ src/components/podcasts/PodcastLibrary.tsx
@@ -41,9 +41,7 @@ export default function PodcastLibrary({
 
   return (
     <main
-      className={cn("p-4 sm:p-6 lg:p-8", {
-        "pb-28 md:pb-8": currentTrack,
-      })}
+      className={cn("p-4 sm:p-6 lg:p-8", "pb-28 md:pb-8")}
     >
       {showTitle && (
         <h1 className="font-headline mb-6 text-3xl font-bold tracking-tight">
