238d52f8ef295b49332c102a9cbd22d205cc6783
vjsdk <contact.mnrfrom2020@gmail.com>
2026-06-17 00:20:52 +0600

    PWA Support

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      includeAssets: ["favicon.png", "mnr.svg"],
       manifest: {
         name: "Talk - Audio Streaming",
         short_name: "Talk",
